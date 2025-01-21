9ff438ed5fb52890209568c3f25e747cbef181b1
Daniel Velloso <131708906+vellloso@users.noreply.github.com>
2025-01-21 17:13:15 -0300

    first PWA config

--- src/main.tsx
+++ src/main.tsx
@@ -7,3 +7,13 @@ createRoot(document.getElementById("root")!).render(
     <App />
   </StrictMode>
 );
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    }).catch(error => {
+      console.log('Service Worker registration failed:', error);
+    });
+  });
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'Elba',
+        short_name: 'Elba',
+        description: 'A web application for comparing gasoline and ethanol prices based on the fuel consumption of the user\'s car.',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#007bff',
+        icons: [
+          {
+            src: '/public/icons/iconTest192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/public/icons/iconTest512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ],
+        lang: 'en-US',
+      }
+    })
+  ]
+});
